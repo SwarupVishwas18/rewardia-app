754050d5f77be123aedbe192f92ef739948e1fe8
Swarup Vishwas <swarupvishwas000@gmail.com>
2025-06-24 19:29:05 +0530

    login and signup design changes

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,20 +2,25 @@ function Login() {
     return (
         <section className="center-flex">
             <div className="form-main">
-                <h1>Login</h1>
-                <div className="input-main">
-                    <label htmlFor="email">Email Address : </label>
-                    <input type="email" name="email" id="email" />
+                <div className="ls">
+                    <h1>Login</h1>
+                        <div className="input-main">
+                            <label htmlFor="email">Email Address : </label>
+                            <input type="email" name="email" id="email" />
+                        </div>
+                        <div className="input-main">
+                            <label htmlFor="password">Password : </label>
+                            <input type="password" name="password" id="password" />
+                        </div>
+                    <div className="btns-main">
+                        <button className="submit-btn">Submit</button>
+                        <a href="/">
+                            <button className="secondary-btn">Go Back</button>
+                        </a>
+                    </div>
                 </div>
-                <div className="input-main">
-                    <label htmlFor="password">Password : </label>
-                    <input type="password" name="password" id="password" />
-                </div>
-                <div className="btns-main">
-                    <button className="submit-btn">Submit</button>
-                    <a href="/">
-                        <button className="secondary-btn">Go Back</button>
-                    </a>
+                <div className="rs">
+                    <img src="/signup.png" alt="" />
                 </div>
             </div>
         </section>
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -2,32 +2,39 @@ function Signup() {
     return (
         <section className="center-flex">
             <div className="form-main">
-                <h1>Signup</h1>
-                <div className="input-main">
-                    <label htmlFor="name">Name : </label>
-                    <input type="text" name="name" id="name" />
+                <div className="ls">
+                    <h1>Signup</h1>
+                    <div className="input-container">
+                        <div className="input-main">
+                            <label htmlFor="name">Name  </label>
+                            <input type="text" name="name" id="name" />
+                        </div>
+                        <div className="input-main">
+                            <label htmlFor="email">Email Address  </label>
+                            <input type="email" name="email" id="email" />
+                        </div>
+                        <div className="input-main">
+                            <label htmlFor="userid">User Id  </label>
+                            <input type="text" name="userid" id="userid" />
+                        </div>
+                        <div className="input-main">
+                            <label htmlFor="password">Password  </label>
+                            <input type="password" name="password" id="password" />
+                        </div>
+                        <div className="input-main">
+                            <label htmlFor="re-password">Retype Password  </label>
+                            <input type="password" name="re-password" id="re-password" />
+                        </div>
+                    </div>
+                    <div className="btns-main">
+                        <button className="submit-btn">Submit</button>
+                        <a href="/">
+                            <button className="secondary-btn">Go Back</button>
+                        </a>
+                    </div>
                 </div>
-                <div className="input-main">
-                    <label htmlFor="email">Email Address : </label>
-                    <input type="email" name="email" id="email" />
-                </div>
-                <div className="input-main">
-                    <label htmlFor="userid">User Id : </label>
-                    <input type="text" name="userid" id="userid" />
-                </div>
-                <div className="input-main">
-                    <label htmlFor="password">Password : </label>
-                    <input type="password" name="password" id="password" />
-                </div>
-                <div className="input-main">
-                    <label htmlFor="re-password">Retype Password : </label>
-                    <input type="password" name="re-password" id="re-password" />
-                </div>
-                <div className="btns-main">
-                    <button className="submit-btn">Submit</button>
-                    <a href="/">
-                        <button className="secondary-btn">Go Back</button>
-                    </a>
+                <div className="rs">
+                    <img src="/signup.png" alt="" />
                 </div>
             </div>
         </section>
