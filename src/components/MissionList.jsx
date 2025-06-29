1c680e5148c6afdd059874f54f115e1b868d6cca
Swarup Vishwas <swarupvishwas000@gmail.com>
2025-06-29 12:01:42 +0530

    #11 - frontend-feat: add mission button

--- src/components/MissionList.jsx
+++ src/components/MissionList.jsx
@@ -14,6 +14,9 @@ function MissionList({ missions, setActiveMission }) {
                 <div className="mission-link"><a href="">Mission</a></div>
                 <div className="mission-link"><a href="">Mission</a></div>
             </div>
+            <div className="btns-main">
+                <button>Add Mission</button>
+            </div>
         </section>
     )
 }
